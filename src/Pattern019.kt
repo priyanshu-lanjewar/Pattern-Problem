3e5ed37b65fd76544e9e0e85f68ea6a2cdd255fd
duggu <rj.priyanshu@aol.com>
2020-07-26 22:27:05 +0530

    Programe 19

---
+++ src/Pattern019.kt
@@ -0,0 +1,18 @@
+/*output
+
+ A  B  C  D  E
+ A  B  C  D
+ A  B  C
+ A  B
+ A
+
+*/
+
+fun main() {
+    println("Pattern 17")
+    for (i in 1..5){
+        for (j in 1 .. (6-i))
+            print(" ${(j+64).toChar()} ")
+        println()
+    }
+}
